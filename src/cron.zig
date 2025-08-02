838c4ceed1d27bcae0f80fcf5c96b348db1ca31f
Güney Saramalı <guneysaramali@live.co.uk>
2025-08-02 09:15:58 +0300

    Fix type

--- src/cron.zig
+++ src/cron.zig
@@ -63,7 +63,7 @@ pub const Cron = struct {
         };
     }
 
-    pub fn get_datetime(_: *Self) @type(datetime) {
+    pub fn get_datetime(_: *Self) @TypeOf(datetime) {
         return datetime;
     }
 
