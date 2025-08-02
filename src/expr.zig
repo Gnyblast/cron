c74745f5850c433f756b05629b6017041cba5ba1
Güney Saramalı <guneysaramali@live.co.uk>
2025-08-02 09:38:47 +0300

    intcast for isSet for mcu's

--- src/expr.zig
+++ src/expr.zig
@@ -376,7 +376,7 @@ pub const CronField = struct {
         }
 
         if (self.tag == .YEAR_OFFSET) {
-            return self.allowed.isSet(value - EPOCH);
+            return self.allowed.isSet(@as(usize, @intCast(value))) - EPOCH);
         }
         return self.allowed.isSet(value);
     }
